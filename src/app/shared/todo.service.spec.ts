042a3d508c6469362c02f7dc4dcebffe563992f9
hollannikas <hollannikas@users.noreply.github.com>
2017-03-22 11:35:13 +0200

    Added indexes to TODOs

--- src/app/shared/todo.service.spec.ts
+++ src/app/shared/todo.service.spec.ts
@@ -15,16 +15,27 @@ describe('TodoService', () => {
     expect(service.getTodos()).toContain(todo);
   }));
 
+  it('should add an index when adding a todo', inject([TodoService], (service: TodoService) => {
+    const todo = { description: 'bob', done: false};
+    const lengthBeforeAdding = service.getTodos().length;
+    expect(todo['index']).toBeFalsy();
+    service.addTodo(todo);
+    expect(todo['index']).toBeTruthy();
+    expect(todo['index']).toEqual(lengthBeforeAdding);
+  }));
+
   it('should remove a cloned todo from the correct index', inject([TodoService], (service: TodoService) => {
     const original = { description: 'bob', done: false};
     const clone = Object.assign({}, original);
     const startIndex = service.getTodos().length;
     service.addTodo(original);
     service.addTodo({ description: 'elisa', done: false});
+    expect(clone['index']).toBeFalsy();
     service.addTodo(clone);
+    expect(clone['index']).toBeTruthy();
     service.removeTodo(clone);
     const todos = service.getTodos();
-    expect(todos[startIndex+1]).toBe(original);
+    expect(todos[startIndex]).toBe(original);
   }));
 
 });
--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -12,7 +12,10 @@ export class TodoService {
 
   constructor() {
     // This simulates getting data from a restful service
-    TODOS.forEach((todo) => this.todos.push(todo));
+    TODOS.forEach((todo, index) => {
+      todo['index'] = index;
+      this.todos.push(todo);
+    });
   }
 
   getTodos(){
@@ -21,13 +24,21 @@ export class TodoService {
 
   addTodo(todo) {
     this.todos.push(todo);
+    this.reindex();
+  }
+
+  private reindex() {
+    // reindex
+    this.todos.forEach((item, index) => item.index = index);
   }
 
   removeTodo(todo) {
-    const index = this.todos.findIndex(item => item === todo);
+    const index = this.todos.findIndex(item => item.index === todo.index);
+    console.log(index);
     if(index != -1) {
       this.todos.splice(index, 1);
     }
+    this.reindex();
   }
 
 }
